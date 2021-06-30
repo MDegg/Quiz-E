a7b9b73c9e637805ed0c76f7828b44cc65e16e55
MDegg <matt.degg@outlook.com>
2021-06-30 12:59:35 +0000

    added layout of quiz page and styled

---
+++ assets/java/script.js
@@ -0,0 +1,18 @@
+//function startGame();
+
+
+
+
+function showQuestions(){
+  let quizQuestions = getElementById("quiz-questions")
+  quizQuestions.innerhtml = 
+      (`<div>
+          hello!
+      </div>`)
+  ;
+
+}
+
+//function getAnswers();
+
+function showQuestions();
