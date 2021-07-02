0a92803810740b481dc662ecfac9a415055bbf50
MDegg <matt.degg@outlook.com>
2021-07-02 11:25:05 +0000

    added more styling to quiz. Added js function to show question and answer boxes. debugging using google dev tools

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,16 +1,29 @@
-let startButton= document.getElementById("start-quiz")    
+let startButton= document.getElementById("start-quiz") 
+let questionElement= document.getElementById('quiz-questions')   
+let shuffleQuestions, currentQuestionIndex
+//let questionElement = document.getElementById('quiz-question')
+let answerButtons = document.getElementById('answer-btn')
 
-startButton.addEventListener('click', buildQuiz)
+startButton.addEventListener('click', buildQuiz);
+
  function buildQuiz(){
-   console.log("Start Game!")
-      
- 
-      
+   console.log("Start Game!");
+   startButton.classList.add('hide');
+   shuffleQuestions = questions.sort(() => Math.random() - .5);
+   currentQuestionIndex = 0
+   questionElement.classList.remove('hide');
+   answerButtons.classList.remove('hide');
+   nextQuestion()
+        
    }
 
  function nextQuestion(){
+   showQuestion(shuffleQuestions[currentQuestionIndex])
 
+ }
 
+ function showQuestion(questions){
+  questionElement.innerText = questions.question;
  }
 
 
@@ -18,3 +31,15 @@ startButton.addEventListener('click', buildQuiz)
 
 
  }
+
+ const questions = [
+ {
+  question : 'What was the first commercially successful video game?',
+  answer: [
+    { text: 'Pong', correct: true},
+    { text: 'Pac Man', correct: false}
+  ]
+
+ }
+
+ ]
